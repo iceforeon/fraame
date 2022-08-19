837851fc538485c86dc36b48854088ec34c6e5f5
iceforeon <iceforeon@gmail.com>
2022-08-19 15:27:50 +0800

    wip

--- app/Http/Livewire/Movies/Stats.php
+++ app/Http/Livewire/Movies/Stats.php
@@ -9,7 +9,7 @@ class Stats extends Component
     public function render()
     {
         return <<<'blade'
-            <a href="{{ route('animes.index') }}" class="px-4 py-5 bg-white shadow-sm hover:shadow-lg focus:shadow-lg rounded-sm overflow-hidden sm:p-6 transition ease-in-out duration-150 focus:outline-none">
+            <a href="{{ route('movies.index') }}" class="px-4 py-5 bg-white shadow-sm hover:shadow-lg focus:shadow-lg rounded-sm overflow-hidden sm:p-6 transition ease-in-out duration-150 focus:outline-none">
                 <dt class="text-sm font-medium text-gray-500 truncate">Total Movies</dt>
                 <dd class="mt-1 text-3xl tracking-tight font-semibold text-gray-900">{{ \App\Models\Movie::count() }}</dd>
             </a>
